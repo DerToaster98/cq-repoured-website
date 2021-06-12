73a8853dd5ce4e09047ec50020bb46f0c91caa91
DerToaster98 <seiler.oli@web.de>
2021-06-12 22:33:25 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import DiscordProfilePicture from './components/discordProfilePicture/discordProfilePicture';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      
     </div>
   );
 }
