a25780bb1ef6ff2a3dca983f3bc791792cda7d6d
DerToaster98 <seiler.oli@web.de>
2021-07-01 16:19:48 +0200

    create navbar component

---
+++ src/components/header/NavBar.js
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+import { NavLink, Link, BrowserRouter } from 'react-router-dom';
+
+import './navBar.css';
+
+export default class NavBar extends Component {
+
+    render() {
+        return (
+           <nav>
+                <BrowserRouter className="navbar">
+                    <NavLink className="navbarItem" activeClassName="navbarItem active" to="/home" exact><div>Main Page</div></NavLink>
+                    <NavLink className="navbarItem" activeClassName="navbarItem active" to="/changelog"><div>Changelog</div></NavLink>
+                    <NavLink className="navbarItem" activeClassName="navbarItem active" to="/downloads" exact><div>Downloads</div></NavLink>
+                    <NavLink className="navbarItem" activeClassName="navbarItem active" to="/team" exact><div>The Team</div></NavLink>
+                    <a className="navbarItem" href="http://cqr-wiki.org" target="_blank"><div>Wiki</div></a>
+                    <NavLink className="navbarItem" activeClassName="navbarItem active" to="/theteam" exact><div>Player Made Dungeons</div></NavLink>
+                </BrowserRouter>
+           </nav>
+        );
+    }
+
+}
