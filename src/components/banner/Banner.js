8365b6986c9123b44077ee996fb144a9e268c387
DerToaster98 <seiler.oli@web.de>
2021-07-01 16:19:57 +0200

    create banner component

---
+++ src/components/banner/Banner.js
@@ -0,0 +1,26 @@
+import { Component } from "react";
+
+import background from "../../assets/img/background.jpg";
+
+import "./banner.css";
+
+export default class Banner extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            displayText: this.props.displayText
+        }
+    }
+
+    render() {
+        return(
+            <div className="banner-container" style={{backgroundImage: `url(${background})`}}>
+                <div className="banner-text">
+                    <span>{this.state.displayText}</span>
+                </div>
+            </div>
+        );
+    }
+}
