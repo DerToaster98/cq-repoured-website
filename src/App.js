bb233f72a0ecfc5f1a86dfcc5399bdfd09360de3
DerToaster98 <seiler.oli@web.de>
2021-07-01 17:12:47 +0200

    add in homepage

--- src/App.js
+++ src/App.js
@@ -6,37 +6,30 @@ import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import Logo from './components/header/Logo'
 import NavBar from './components/header/NavBar'
 
+import Home from './pages/home/Home';
+
 function App() {
   return (
     <div className="App">
-      <table className="content-table">
-        <tbody>
-          <tr>
-            <th className="bg-side"></th>
-            <th className="bg-center">
-              <Router>
-                <Route path='/'>
-                  <Logo></Logo>
-                  <NavBar className="centered"></NavBar>
-                </Route>
+      
+      <Router>
+        <Route path='/'>
+          <Logo></Logo>
+          <NavBar className="centered"></NavBar>
+        </Route>
 
-                <Route path='/' exact>
-                  <Redirect to='/home'></Redirect>
-                </Route>
+        <Route path='/' exact>
+          <Redirect to='/home'></Redirect>
+        </Route>
 
-                {/*
-                <Route path='/home' exact component={}></Route>
-                <Route path='/changelog/:changelogID' component={}></Route>
-                <Route path='/downloads' exact component={}></Route>
-                <Route path='/team' exact component={}></Route>
-                <Route path='/pmd' exact component={}></Route>
-                */}
-              </Router>
-            </th>
-            <th className="bg-side"></th>
-          </tr>
-        </tbody>
-      </table>
+        
+        <Route path='/home' exact component={Home}></Route>
+        {/*<Route path='/changelog/:changelogID' component={}></Route>
+        <Route path='/downloads' exact component={}></Route>
+        <Route path='/team' exact component={}></Route>
+        <Route path='/pmd' exact component={}></Route>
+        */}
+      </Router>
       
     </div>
   );
