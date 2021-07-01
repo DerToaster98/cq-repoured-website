994b5752fc4ef79bb20c3f5cea626687b0ddb304
DerToaster98 <seiler.oli@web.de>
2021-07-01 16:41:28 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-import DiscordProfilePicture from './components/discordProfilePicture/discordProfilePicture';
+
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+
+import Logo from './components/header/Logo'
+import NavBar from './components/header/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table className="content-table">
+        <tbody>
+          <tr>
+            <th className="bg-side"></th>
+            <th className="bg-center">
+              <Router>
+                <Route path='/'>
+                  <Logo></Logo>
+                  <NavBar className="centered"></NavBar>
+                </Route>
+
+                <Route path='/' exact>
+                  <Redirect to='/home'></Redirect>
+                </Route>
+
+                {/*
+                <Route path='/home' exact component={}></Route>
+                <Route path='/changelog/:changelogID' component={}></Route>
+                <Route path='/downloads' exact component={}></Route>
+                <Route path='/team' exact component={}></Route>
+                <Route path='/pmd' exact component={}></Route>
+                */}
+              </Router>
+            </th>
+            <th className="bg-side"></th>
+          </tr>
+        </tbody>
+      </table>
       
     </div>
   );
