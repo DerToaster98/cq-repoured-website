09e6918c6c18b515d9c9662b5f37a9d4c43b9ee0
DerToaster98 <seiler.oli@web.de>
2021-07-01 23:18:57 +0200

    patch navbar on gh-p

--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import { NavLink, Link, BrowserRouter } from 'react-router-dom';
+import { NavLink, Link, HashRouter } from 'react-router-dom';
 
 import './navBar.css';
 
@@ -8,14 +8,14 @@ export default class NavBar extends Component {
     render() {
         return (
            <nav>
-                <BrowserRouter className="navbar">
+                <HashRouter className="navbar" basename="/">
                     <NavLink className="navbarItem" activeClassName="navbarItem active" to="/" exact><div>Main Page</div></NavLink>
                     <NavLink className="navbarItem" activeClassName="navbarItem active" to="/changelog"><div>Changelog</div></NavLink>
                     <NavLink className="navbarItem" activeClassName="navbarItem active" to="/downloads" exact><div>Downloads</div></NavLink>
                     <NavLink className="navbarItem" activeClassName="navbarItem active" to="/team" exact><div>The Team</div></NavLink>
                     <a className="navbarItem" href="http://cqr-wiki.org" target="_blank"><div>Wiki</div></a>
                     <NavLink className="navbarItem" activeClassName="navbarItem active" to="/pmd" exact><div>Player Made Dungeons</div></NavLink>
-                </BrowserRouter>
+                </HashRouter>
            </nav>
         );
     }
