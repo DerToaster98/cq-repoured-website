ff74a81ff60e999e89f497026f70295abaa4e60c
DerToaster98 <seiler.oli@web.de>
2021-07-09 16:41:52 +0200

    slideshow images

---
+++ src/pages/home/Slideshows.js
@@ -0,0 +1,88 @@
+const SLIDE_PATH_PREFIX = '../../assets/slideshow/';
+
+export const SLIDES_CASTLES = [
+    SLIDE_PATH_PREFIX + 'castles/1.jpg',
+    SLIDE_PATH_PREFIX + 'castles/2.jpg',
+    SLIDE_PATH_PREFIX + 'castles/3.jpg',
+    SLIDE_PATH_PREFIX + 'castles/4.jpg',
+    SLIDE_PATH_PREFIX + 'castles/5.jpg',
+    SLIDE_PATH_PREFIX + 'castles/6.jpg',
+    SLIDE_PATH_PREFIX + 'castles/7.jpg',
+    SLIDE_PATH_PREFIX + 'castles/8.jpg',
+    SLIDE_PATH_PREFIX + 'castles/9.jpg',
+    SLIDE_PATH_PREFIX + 'castles/10.jpg'
+]
+
+export const SLIDES_VOLCANO = [
+    SLIDE_PATH_PREFIX + 'volcano/1.jpg',
+    SLIDE_PATH_PREFIX + 'volcano/2.jpg',
+    SLIDE_PATH_PREFIX + 'volcano/3.jpg',
+    SLIDE_PATH_PREFIX + 'volcano/4.jpg'
+]
+
+export const SLIDES_CASTLES_SNOW = [
+    SLIDE_PATH_PREFIX + 'castles_snow/1.jpg',
+    SLIDE_PATH_PREFIX + 'castles_snow/2.jpg',
+    SLIDE_PATH_PREFIX + 'castles_snow/3.jpg',
+    SLIDE_PATH_PREFIX + 'castles_snow/4.jpg',
+    SLIDE_PATH_PREFIX + 'castles_snow/5.jpg',
+    SLIDE_PATH_PREFIX + 'castles_snow/6.jpg',
+    SLIDE_PATH_PREFIX + 'castles_snow/7.jpg',
+    SLIDE_PATH_PREFIX + 'castles_snow/8.jpg',
+    SLIDE_PATH_PREFIX + 'castles_snow/9.jpg'
+]
+
+export const SLIDES_STRONGHOLDS = [
+    SLIDE_PATH_PREFIX + 'strongholds/1.jpg',
+    SLIDE_PATH_PREFIX + 'strongholds/2.jpg',
+    SLIDE_PATH_PREFIX + 'strongholds/3.jpg',
+    SLIDE_PATH_PREFIX + 'strongholds/4.jpg',
+    SLIDE_PATH_PREFIX + 'strongholds/5.jpg',
+    SLIDE_PATH_PREFIX + 'strongholds/6.jpg',
+    SLIDE_PATH_PREFIX + 'strongholds/7.jpg',
+    SLIDE_PATH_PREFIX + 'strongholds/8.jpg',
+    SLIDE_PATH_PREFIX + 'strongholds/9.jpg'
+]
+
+export const SLIDES_OUTPOSTS = [
+    SLIDE_PATH_PREFIX + 'outposts/1.jpg',
+    SLIDE_PATH_PREFIX + 'outposts/2.jpg',
+    SLIDE_PATH_PREFIX + 'outposts/3.jpg',
+    SLIDE_PATH_PREFIX + 'outposts/4.jpg',
+    SLIDE_PATH_PREFIX + 'outposts/5.jpg',
+    SLIDE_PATH_PREFIX + 'outposts/6.jpg',
+    SLIDE_PATH_PREFIX + 'outposts/7.jpg'
+]
+
+export const SLIDES_TAVERNS = [
+
+]
+
+export const SLIDES_NETHER = [
+    SLIDE_PATH_PREFIX + 'nether/1.jpg',
+    SLIDE_PATH_PREFIX + 'nether/2.jpg',
+    SLIDE_PATH_PREFIX + 'nether/3.jpg',
+    SLIDE_PATH_PREFIX + 'nether/4.jpg',
+    SLIDE_PATH_PREFIX + 'nether/5.jpg'
+]
+
+export const SLIDES_SHIPS = [
+    SLIDE_PATH_PREFIX + 'ships/1.jpg',
+    SLIDE_PATH_PREFIX + 'ships/2.jpg',
+    SLIDE_PATH_PREFIX + 'ships/3.jpg',
+    SLIDE_PATH_PREFIX + 'ships/4.jpg',
+    SLIDE_PATH_PREFIX + 'ships/5.jpg',
+    SLIDE_PATH_PREFIX + 'ships/6.jpg',
+    SLIDE_PATH_PREFIX + 'ships/7.jpg',
+    SLIDE_PATH_PREFIX + 'ships/8.jpg',
+    SLIDE_PATH_PREFIX + 'ships/9.jpg'
+]
+
+export const SLIDES_CAVES = [
+    SLIDE_PATH_PREFIX + 'caves/1.jpg',
+    SLIDE_PATH_PREFIX + 'caves/2.jpg'
+]
+
+export const SLIDES_WALL = [
+    SLIDE_PATH_PREFIX + 'wall/1.jpg'
+]
