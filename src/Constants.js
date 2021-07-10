dc05cf7cc5ba581433f0b7cac1e792cf636e5c60
DerToaster98 <seiler.oli@web.de>
2021-07-10 13:59:43 +0200

    add helper methods to access files in public directory

--- src/Constants.js
+++ src/Constants.js
@@ -1,1 +1,5 @@
-export const CQR_GITHUB_REPOSITORY_URL = "https://github.com/TeamChocoQuest/ChocolateQuestRepoured"
+export const CQR_GITHUB_REPOSITORY_URL = "https://github.com/TeamChocoQuest/ChocolateQuestRepoured";
+export const SITE_BASE_URL = "cq-repoured-website";
+export function getSiteBaseURL() {
+    return window.location.origin + "/" + SITE_BASE_URL;
+} 
