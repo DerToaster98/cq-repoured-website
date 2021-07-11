15e4295418fc8a2e71c2b8fd90c8dcea81317780
DerToaster98 <seiler.oli@web.de>
2021-07-11 19:32:58 +0200

    add lazy background loading

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+
 export const CQR_GITHUB_REPOSITORY_URL = "https://github.com/TeamChocoQuest/ChocolateQuestRepoured";
 export const SITE_BASE_URL = "cq-repoured-website";
 export function getSiteBaseURL() {
     return window.location.origin + "/" + SITE_BASE_URL;
 } 
+
+export function LazyBackground({src}) {
+    const [source, setSource] = useState(getSiteBaseURL() + '/loading.gif');
+
+    useEffect(() => {
+        const img = new Image();
+        img.src = src;
+        img.onload = () => setSource(src);
+    }, [src]);
+
+    return(
+        <div style={{backgroundImage: `url(${source})`}}></div>
+    )
+}
