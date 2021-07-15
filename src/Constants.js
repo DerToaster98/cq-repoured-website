32d0124478b1cc6ad800dacaf635fd5852f34abc
DerToaster98 <seiler.oli@web.de>
2021-07-15 17:32:11 +0200

    add helper method to generate lazyImage

--- src/Constants.js
+++ src/Constants.js
@@ -1,10 +1,21 @@
 import { useEffect, useState } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 export const CQR_GITHUB_REPOSITORY_URL = "https://github.com/TeamChocoQuest/ChocolateQuestRepoured";
 export const SITE_BASE_URL = "cq-repoured-website";
 export function getSiteBaseURL() {
     return window.location.origin + "/" + SITE_BASE_URL;
 } 
+
+export function generateLazyBackgroundWithJustImagePath(imagePath) {
+    let urlsrc = getSiteBaseURL() + "/" + imagePath;
+    let image = new Image();
+    image.src = urlsrc;
+
+    return(
+        <LazyLoadImage alt={image.alt} src={urlsrc}></LazyLoadImage>
+    );
+}
 
 export function LazyBackground({src}) {
     const [source, setSource] = useState(getSiteBaseURL() + '/loading.gif');
