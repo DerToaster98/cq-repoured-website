35df77f0a99caa7193cf71b9f4d7ff5b0e046c82
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2022-12-12 19:23:58 +0100

    preparation for mobile friendly hamburger menu

--- src/components/header/NavBar.js
+++ src/components/header/NavBar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink, HashRouter } from 'react-router-dom';
+import {BrowserView, MobileView} from 'react-device-detect';
 
 import './navBar.css';
 
@@ -7,16 +8,18 @@ export default class NavBar extends Component {
 
     render() {
         return (
-           <nav>
-                <HashRouter className="navbar" basename="/">
-                    <NavLink className="navbarItem" activeClassName="navbarItem active" to="/" exact><div>Main Page</div></NavLink>
-                    <NavLink className="navbarItem" activeClassName="navbarItem active" to="/changelog"><div>Changelog</div></NavLink>
-                    <NavLink className="navbarItem" activeClassName="navbarItem active" to="/downloads" exact><div>Downloads</div></NavLink>
-                    <NavLink className="navbarItem" activeClassName="navbarItem active" to="/team" exact><div>The Team</div></NavLink>
-                    <a className="navbarItem" href="http://cqr-wiki.org" target="_blank" rel="noreferrer"><div>Wiki</div></a>
-                    <NavLink className="navbarItem" activeClassName="navbarItem active" to="/pmd" exact><div>Player Made Dungeons</div></NavLink>
-                </HashRouter>
-           </nav>
+           <BrowserView>
+                <nav>
+                    <HashRouter className="navbar" basename="/">
+                        <NavLink className="navbarItem" activeClassName="navbarItem active" to="/" exact><div>Main Page</div></NavLink>
+                        <NavLink className="navbarItem" activeClassName="navbarItem active" to="/changelog"><div>Changelog</div></NavLink>
+                        <NavLink className="navbarItem" activeClassName="navbarItem active" to="/downloads" exact><div>Downloads</div></NavLink>
+                        <NavLink className="navbarItem" activeClassName="navbarItem active" to="/team" exact><div>The Team</div></NavLink>
+                        <a className="navbarItem" href="http://cqr-wiki.org" target="_blank" rel="noreferrer"><div>Wiki</div></a>
+                        <NavLink className="navbarItem" activeClassName="navbarItem active" to="/pmd" exact><div>Player Made Dungeons</div></NavLink>
+                    </HashRouter>
+                </nav>
+           </BrowserView>
         );
     }
 
