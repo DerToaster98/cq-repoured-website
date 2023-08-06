8c872dac265b9f753eb63cfcc87820d171b16a73
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2023-08-06 12:13:23 +0200

    add trufflehistorian

--- src/pages/team/Team.js
+++ src/pages/team/Team.js
@@ -19,6 +19,7 @@ const DESCRIPTION_XYRON = <span>Xyron was a Frontend developer and UI engineer w
 const DESCRIPTION_PION_STEAM = <span> Pion Steam played CQC when it was previously known as "Better Dungeons." Some time after discovering the mod was being revived, he joined the CQR team as the "Noisemaker." (Sound maker)<br></br>He had a hand in tweaking this site while being another moderator on our discord.</span>
 const DESCRIPTION_SILENTINE = <span>Makes low poly, pixel art models and textures. Worked mainly on remaking the item models/textures and mob textures of the mod. He also made a mod called Grimoire of Gaia.</span>
 const DESCRIPTION_SIR_SQUIDLY = <span>Sir Squidly is one of the earliest artists to join the team, creating pixel art textures and models. Primarily did most of the Block textures, some entity models and textures, and various misc graphics. Is also a modder of various other video games.</span>
+const DESCRIPTION_TRUFFLEHISTORIAN = <span>Dungeon designer and builder for the CQR team.</span>
 
 const DESCRIPTION_SCOOPTWOO = <span>Scooptwoo is a moderator on our discord and helps out answering questions.</span>
 const DESCRIPTION_TURBULATION = <span>Turbulation is a person that has collected information about old chocolate quest versions and has set up an archive for those.</span>
@@ -76,6 +77,9 @@ export default class Team extends Component {
                         </div>
                         <div className="member-card">
                             <UserContainer username='Sir Squidly' imageSrc={IMAGE_PATH_PREFIX + "sir_squidly.png"} userinfo={DESCRIPTION_SIR_SQUIDLY} userlinks={null}></UserContainer>
+                        </div>
+                        <div className="member-card">
+                            <UserContainer username="TruffleHistorian" imageSrc={IMAGE_PATH_PREFIX + "trufflehistorian.png"} userinfo={DESCRIPTION_TRUFFLEHISTORIAN} userlinks={null}></UserContainer>
                         </div>
                     </div>
                     <h1 className="heading">Notable helping hands</h1>    
